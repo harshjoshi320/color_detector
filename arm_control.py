d91a31e15468ad73c4f87d4c442602c2b6c73f48
Harsh Joshi <harshjoshibpl@gmail.com>
2020-03-07 09:36:10 +0530

    Added arm controller module

---
+++ arm_control.py
@@ -0,0 +1,19 @@
+from orangepwm import *
+from orangeservo import Servo
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+class Arm():
+	def __init__(self, claw, wrist, elbow, shoulder):
+		self.claw = claw 
+		self.wrist = wrist
+		self.elbow = elbow
+		self.shoulder = shoulder
+
+	def clawOpen(self):
+		self.claw.setAngle(0)
+		self.claw.stop()
+
+	def clawClose(self):
+		self.claw.setAngle(55)
+		self.claw.stop

21ccbd3038c9587f8398792a4b7c48dd1d17df00
Harsh Joshi <harshjoshibpl@gmail.com>
2020-03-15 06:35:27 +0000

    Adjusted claw and elbow angles, added default port values for Arm, added destructor method to close the object gracefully

--- arm_control.py
+++ arm_control.py
@@ -4,23 +4,38 @@ from pyA20.gpio import gpio
 from pyA20.gpio import port
 from time import sleep
 
+
 class Arm():
-    def __init__(self, ports):
+    def __init__(self, ports=[ port.PA6, port.PA12, port.PA3, port.PA11]):
         gpio.init()
         self.claw = Servo(ports[0])
         self.wrist = Servo(ports[1])
         self.elbow = Servo(ports[2])
         self.shoulder = Servo(ports[3])
 
+    def __del__(self):
+        del self.claw
+        del self.wrist
+        del self.elbow
+        del self.shoulder
 
-    def initialize(self):
-        print(f"[INFO] Initializing...")
+
+    def initialize(self, verb=0):
+        if verb != 0:
+            print(f"[INFO] Initializing...")
         self.claw.setAngle(0); sleep(0.5)
         self.wrist.setAngle(0); sleep(0.5)
         self.elbow.setAngle(0); sleep(0.5)
         self.stopAll()
-        print(f"    ...All servos in initial positions.")
+        if verb != 0:
+            print(f"    ...servos in initial positions.")
         # self.shoulder.setAngle(25); sleep(1)
+
+
+    def fullSweep(self):
+        self.shoulder.setAngle(0);sleep(1)
+        self.shoulder.setAngle(180);sleep(1)
+        self.shoulder.setAngle(25);sleep(1)
 
 
     def stopAll(self):
@@ -75,7 +90,7 @@ class Arm():
         self.claw.setAngle(0)
         print("Position: 0")
         sleep(1)
-        self.claw.setAngle(60)
+        self.claw.setAngle(90)
         print("Position: 60")
         sleep(2)
         self.claw.setAngle(0)
@@ -87,37 +102,40 @@ class Arm():
     
     def pick(self, pos=0):
         print(f"[INFO] Start Routine: Pick At Pos-{pos}... ", end='')
-        positions = [0, 24, 50]
-        self.initialize()
+        positions = [0, 29, 48]
+        self.initialize(0)
         self.shoulder.setAngle(positions[pos]); sleep(1)
         self.shoulder.stop()
         self.claw.setAngle(0); sleep(1)
         self.wrist.setAngle(0); sleep(1)
         self.elbow.setAngle(70); sleep(1)
-        self.claw.setAngle(50); sleep(1)
+        self.claw.setAngle(90); sleep(1)
         self.wrist.stop()
         self.elbow.setAngle(0); sleep(1)
         self.elbow.stop()
         print("Picked")
 
 
-    def drop(self):
+    def drop(self, pos=2):
+        positions = [75, 95, 120]
         print(f"[INFO] Start Routine: Drop... ", end='')
         self.wrist.setAngle(0); sleep(1)
-        self.shoulder.setAngle(120); sleep(1)
+        self.shoulder.setAngle(positions[pos]); sleep(1)
         self.shoulder.stop()
         self.elbow.setAngle(40); sleep(1)
         self.claw.setAngle(0); sleep(0.5)
-        self.initialize()
+        self.initialize(0)
         print("Dropped")
 
 
 
 if __name__ == '__main__':
+
+
     gpio.init()
     arm = Arm([ port.PA6, port.PA12, port.PA3, port.PA11])
     arm.initialize()
-    arm.test()
+    arm.fullSweep()
     # arm.pick(0)
     # arm.drop()
     # arm.stopAll()
@@ -131,4 +149,5 @@ if __name__ == '__main__':
     # arm.stopAll()
     # sleep(2)
     arm.stopAll()
+    del arm
 
